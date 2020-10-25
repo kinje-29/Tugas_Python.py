8bd0fdf204803aa92fdbe2fd3ab0b49dc1c9917f
fajar firmansyah <fajar.vds@gmail.com>
2020-10-25 23:54:40 +0700

    Tugas.Py

---
+++ Tugas.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 20 18:09:26 2020
+
+@author: fajar
+"""
+
+print"++++ Selamat Datang ++++"
+print"======================"
+
+#masukkan variabel
+
+nama= raw_input ("Masuk Kan Nama: ")
+nama2= raw_input ("Masuk Kan Panggilan: ")
+NPM= raw_input ("Masuk kan NPM: ")
+tl= raw_input ("Tempat Lahir: ")
+tgl= raw_input ("Tanggal Lahir: ")
+tlpn= raw_input ("Nomor Telepon: ")
+almt= raw_input ("Alamat Rumah: ")
+
+
+#keluar nya input
+
+print"++++ BIODATA ANDA ++++"
+print"===================="
+
+
+
+print"Hallo",nama,"Nama panggilan kamu",nama2,"NPM kamu adalah",NPM,"KAmu Lahir dari daerah",tl,"kamu lahir pada tanggal",tgl,"nommor telepon kamu adalah",tlpn,"alamat rumah kamu di daerah",almt
